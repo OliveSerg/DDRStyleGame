e8b3c9c1e203aaf48f18866d30062148d0cacc08
Sergio <oliveserg@gmail.com>
2016-09-27 19:35:08 +0000

    route to room

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var joinRoom = document.getElementById('join-room')
 
   createRoom.addEventListener('click', function(event){
-
+    function generateUID() {
+      return ((Math.random()*Math.pow(36,4) << 0).toString(36))
+    }
+    window.location.replace(`${window.location.href + generateUID()}`)
   })
 
   joinRoom.addEventListener('click', function(event){
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/:roomId', function(req, res, next){
+  res.render('index')
+  console.log('work')
+})
+
 module.exports = router;
