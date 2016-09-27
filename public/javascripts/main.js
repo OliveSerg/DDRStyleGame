7da35d7afcb6a24bf937c99f4644572a8b15e08b
Sergio <oliveserg@gmail.com>
2016-09-27 18:54:45 +0000

    home inital layout

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var createRoom = document.getElementById('create-room')
+  var joinRoom = document.getElementById('join-room')
+
+  createRoom.addEventListener('click', function(event){
+
+  })
+
+  joinRoom.addEventListener('click', function(event){
+
+  })
+});
