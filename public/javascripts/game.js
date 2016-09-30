63b9fc77bcdf3c9c36342c1bb929c50c326d002c
Sergio <oliveserg@gmail.com>
2016-09-30 20:05:27 +0000

    keydown event

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,12 +1,12 @@
 var socket = io.connect('http://localhost:3000')
-var room = window.location.pathname.replace('/', '')
+var room = window.location.pathname.replace('/game/', '')
 var start = false
-var gameDiv;
+var gameDiv, answer;
 
 document.addEventListener("DOMContentLoaded", function(event) {
   var gameDiv = document.getElementById('game')
   var roomId = document.getElementById('room-id')
-  roomId.innerHTML = `Your Room Id is ${window.location.pathname.replace('/game/', '')}`
+  roomId.innerHTML = `Your Room Id is ${room}`
 
   var begin = document.getElementById('begin')
   begin.style.visibility = 'hidden'
@@ -16,36 +16,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
     socket.emit('start', room)
   })
 
-  document.addEventListener('onkeypress', function(){
+  document.addEventListener('keydown', function(ev){
     if (start) {
 
     }
+  }, false)
+
+  socket.on('connect', function(){
+    socket.emit('room', room)
   })
+
+  socket.on('begin', function(data){
+    document.getElementById('waiting').style.visibility = 'hidden'
+    var begin = document.getElementById('begin')
+    begin.style.visibility = 'visible'
+  })
+
+  socket.on('start', function(data){
+    start = true
+    var timer = 3;
+    var interval = setInterval(function(){
+      gameDiv.innerHTML = `${timer}`
+      if (timer == 0) {
+        displayOutput(data.key)
+        clearInterval(interval)
+      }
+      timer--
+    }, 1000)
+  })
+
+  function displayOutput(direction){
+    gameDiv.innerHTML = ""
+    gameDiv.innerHTML = direction
+    answer = direction
+  }
 })
-
-socket.on('connect', function(){
-  socket.emit('room', room)
-})
-
-socket.on('begin', function(data){
-  document.getElementById('waiting').style.visibility = 'hidden'
-  var begin = document.getElementById('begin')
-  begin.style.visibility = 'visible'
-})
-
-socket.on('start', function(data){
-  start = true
-  var timer = 3;
-  var interval = setInterval(function(){
-    gameDiv.innerHTML = `${timer}`
-    if (timer == 0) {
-      gameDiv.innerHTML = ""
-      clearInterval(interval)
-    }
-    timer--
-  }, 1000)
-})
-
-function displayOutput(direction){
-
-}
